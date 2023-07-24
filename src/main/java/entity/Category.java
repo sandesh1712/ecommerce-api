f308c5721f2aa502de2b34c498a0af9010b95d40
sandesh sidanale <sandeshsidanale@gmail.com>
2023-07-24 17:08:26 +0530

    added product relations

---
+++ src/main/java/entity/Category.java
@@ -0,0 +1,50 @@
+package entity;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category extends SuperEntity{
+	@Column(name = "name")
+	private String name;
+	
+	@OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
+	@JsonIgnore
+	private List<Product> products; 
+	
+	@Column(name = "desc")
+	private String desc;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+	
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+}
