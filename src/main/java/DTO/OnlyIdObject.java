4609963905c178d7f4e9586201dc96a00a130fd1
sandesh sidanale <sandeshsidanale@gmail.com>
2023-07-26 18:25:02 +0530

    added review controller and DTO to hide irrelevant data

---
+++ src/main/java/DTO/OnlyIdObject.java
@@ -0,0 +1,13 @@
+package DTO;
+
+public class OnlyIdObject {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
---
+++ src/main/java/helper/View.java
@@ -0,0 +1,5 @@
+package helper;
+
+public class View {
+   public interface Summary {}
+}
